c361f51d5335007bf458670ffa9d6195b16944fa
matsu <matsuaki171101@gmail.com>
2024-04-30 14:53:40 +0900

    feat: slider用のサイズにモーダルウィンドウを変更

--- src/components/modal-window/RoomDetail.tsx
+++ src/components/modal-window/RoomDetail.tsx
@@ -5,7 +5,7 @@ import { Slider } from '../slider/Slider'
 
 export const RoomDetail = ({ data, handleClick }: { data: Room[], handleClick: () => void }) => {
     return (
-        <ModalWindow size={SIZE.MEDIUM} handleClick={handleClick}>
+        <ModalWindow size={SIZE.MEDIUM_SLIDER} handleClick={handleClick}>
             <Slider>
                 {data.map((_, i) => (
                     <div className="content-full blue p-6 center" key={i}>
